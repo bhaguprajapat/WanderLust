2593210665408d572575019c2a9db6c6d649f267
bhaguprajapat <bhagwatiprajapat2313@gmail.com>
2025-09-06 12:01:10 +0530

    boilerprint

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs",ejsMate);  
+app.use(express.static(path.join(__dirname,"public")));
 // Database connection
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
