385b9946cbf25b6254e726cd6aee0473f23c376c
bhaguprajapat <bhagwatiprajapat2313@gmail.com>
2025-09-20 16:13:58 +0530

    Session read

--- classroom/server.js
+++ classroom/server.js
@@ -1,28 +1,36 @@
 import express from "express";
 import userRoutes from "./routes/users.js";
 import posts from "./routes/posts.js";
-import cookieParser from "cookie-parser";
+import session from "express-session";
+import flash from "connect-flash";
+import path from "path";
+import { fileURLToPath } from "url";
 const app=express();
-app.use(express.json()); 
 
-app.use(cookieParser());
-// cookies 
-app.get("/getcookies",(req , res)=>{
-    res.cookie("greet","hello");
-    res.send("send you some cookies");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const sessionOptions={
+    secret: "mysupersecretstring",
+    resave:false,
+    saveUninitialized:true,
+};
+app.use(session(sessionOptions));
+app.use(flash());
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+
+app.get("/register", (req,res)=>{
+    let {name="anonouse"}=req.query;
+    req.session.name=name;
+    // res.send(name);
+    req.flash("success", "User registered successfully!");
+    res.redirect("/hello");
 });
-// home root
-app.get("/",(req,res)=>{
-    
-    console.dir(req.cookies);
-    res.send(req.cookies);
-});
-
-
-app.use("/users", userRoutes);// users route
-app.use("/posts", posts);// posts route
-
-
+app.get("/hello", (req , res)=>{
+    res.render("page.ejs",{name:req.session.name, msg:req.flash("success")});
+})
 app.listen(8080,()=>{
     console.log("Server is running on port 8080");
 });
